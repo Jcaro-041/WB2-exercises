c13a693ee2edb3a1e6c60025877536b98ae49da0
Jonathan Caro <jcaro4001@gmail.com>
2023-10-18 11:19:34 -0700

    Condensed code

--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -1,6 +1,5 @@
 // 72/interest rate = number of periods for the investment to double
 savingsAmount = 42000;
-interestRate = .0578;
-interestRatePercent = interestRate * 100;
-console.log(interestRatePercent.toFixed(2));
-console.log("At a " + interestRatePercent.toFixed(2) + "% interest rate, your savings account will be worth " + savingsAmount * 2 + " in " + 72/5.78 + " years." );
+interestRate = 5.78;
+console.log(interestRate.toFixed(2));
+console.log("At a " + interestRate.toFixed(2) + "% interest rate, your savings account will be worth " + savingsAmount * 2 + " in " + 72/5.78 + " years." );
